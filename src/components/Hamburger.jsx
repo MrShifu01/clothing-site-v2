ad7532a30ceaf3c47f3b93819b7f4fa439f89e51
ChristianStander <stander.christian@gmail.com>
2023-05-13 17:54:36 +0200

    hamburger done

---
+++ src/components/Hamburger.jsx
@@ -0,0 +1,14 @@
+import '../index.css'
+
+
+
+// Building the hamburger nav menu for mobile
+export default function HamburgerNav () {
+    return (
+        <div className='hamburger'>
+            <div className='burger one'></div>
+            <div className='burger two'></div>
+            <div className='burger three'></div>
+        </div>
+    )
+}
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,19 @@
 // Using Link for the navigation menu
 import '../index.css'
 import { Link } from 'react-router-dom'
+import HamburgerNav from './Hamburger'
+import { useState } from 'react'
 
 // Building out the navigation bar with a logo and a user icon
 export default function Nav () {
+    // useState to toggle burger menu on and off
+    const [hamburgerOpen, setHamburgeropen] = useState(false)
+
+    // Toggle Function
+    const toggleHamburger = () => {
+        setHamburgeropen(!hamburgerOpen)
+    }
+
     return (
         <>
             <div className="nav-container">
@@ -16,8 +26,10 @@ export default function Nav () {
                         alt="logo"
                         />
                     </a>
-                    <ul className='nav-list'>
-                        <li className="list-item">
+                    {/* Changing the nav list accordingly */}
+                    <ul className={`nav-list ${hamburgerOpen ? "open" : ""}`}>
+                        <li 
+                        className="list-item">
                             <Link 
                             className='nav-link' 
                             to="/">Home</Link>
@@ -49,7 +61,12 @@ export default function Nav () {
                             </Link>
                         </li>
                     </ul>
+                    {/* Bringing in the burger toggle */}
+                    <div className='burger-click' onClick={toggleHamburger}>
+                        <HamburgerNav />
+                    </div>
                 </nav>
+
             </div>
         </>
     )
